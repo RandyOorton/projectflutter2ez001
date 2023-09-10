dda9f07620b501239f5cef8065639a142a17d108
RandyOorton <suparaj38482.prc@gmail.com>
2023-09-11 00:19:03 +0700

    image picker profile screen

--- App/dentist_application/lib/screen/profilescreen.dart
+++ App/dentist_application/lib/screen/profilescreen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -8,12 +11,136 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _FormScreenState extends State<ProfileScreen> {
+  File? imageFile;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SafeArea(
+        child: Scaffold(
       appBar: AppBar(
         title: const Text("หน้าโปรไฟล์"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20.0,
+            ),
+            imageFile == null
+                ? Image.asset(
+                    'assets/no_profile_image.png',
+                    height: 150.0,
+                    width: 150.0,
+                  )
+                : ClipRRect(
+                    borderRadius: BorderRadius.circular(150.0),
+                    child: Image.file(
+                      imageFile!,
+                      height: 200.0,
+                      width: 200.0,
+                      fit: BoxFit.fill,
+                    )),
+            const SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                Map<Permission, PermissionStatus> statuses = await [
+                  Permission.camera,
+                ].request();
+                if (statuses[Permission.camera]!.isGranted) {
+                  showImagePicker(context);
+                } else {
+                  print('no permission provided');
+                }
+                PermissionStatus camerastatus =
+                    await Permission.camera.request();
+
+                if (camerastatus == PermissionStatus.granted) {}
+                if (camerastatus == PermissionStatus.denied) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text("This permission is recommended.")));
+                }
+                if (camerastatus == PermissionStatus.permanentlyDenied) {
+                  openAppSettings();
+                }
+              },
+              child: Text('Select Image'),
+            ),
+          ],
         ),
-    );
+      ),
+    ));
+  }
+
+  final picker = ImagePicker();
+
+  void showImagePicker(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (builder) {
+          return Card(
+            child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 5.2,
+                margin: const EdgeInsets.only(top: 8.0),
+                padding: const EdgeInsets.all(12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                        child: InkWell(
+                      child: const Column(
+                        children: [
+                          Icon(
+                            Icons.image,
+                            size: 60.0,
+                          ),
+                          SizedBox(height: 12.0),
+                          Text(
+                            "Gallery",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 16, color: Colors.black),
+                          )
+                        ],
+                      ),
+                      onTap: ()=> getImage(source: ImageSource.gallery),
+                    )),
+                    Expanded(
+                        child: InkWell(
+                      child: const SizedBox(
+                        child: Column(
+                          children: [
+                            Icon(
+                              Icons.camera_alt,
+                              size: 60.0,
+                            ),
+                            SizedBox(height: 12.0),
+                            Text(
+                              "Camera",
+                              textAlign: TextAlign.center,
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.black),
+                            )
+                          ],
+                        ),
+                      ),
+                      onTap: ()=> getImage(source: ImageSource.camera),
+                    ))
+                  ],
+                )),
+          );
+        });
+  }
+
+  void getImage({required ImageSource source}) async{
+
+    final file = await ImagePicker().pickImage(source: source);
+
+    if(file?.path != null){
+      setState(() {
+        imageFile = File(file!.path);
+      });
+    }
   }
 }
--- App/dentist_application/lib/screen/secondScreen/detailpage.dart
+++ App/dentist_application/lib/screen/secondScreen/detailpage.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:image_picker/image_picker.dart';
 
-
 class DetailPage extends StatefulWidget {
   final DocumentSnapshot patientData;
 
@@ -23,98 +22,102 @@ class _DetailPageState extends State<DetailPage> {
         title: Text('$fname' + ' ' + '$lname'),
         actions: [
           IconButton(
-            icon: const Icon(Icons.add_photo_alternate),
-            onPressed: () async {
+              icon: const Icon(Icons.add_photo_alternate),
+              onPressed: () async {
                 Map<Permission, PermissionStatus> statuses = await [
-                  Permission.storage, Permission.camera,
+                  Permission.storage,
+                  Permission.camera,
                 ].request();
-                if(statuses[Permission.storage]!.isGranted && statuses[Permission.camera]!.isGranted){
+                if (statuses[Permission.storage]!.isGranted &&
+                    statuses[Permission.camera]!.isGranted) {
                   showImagePicker(context);
                 } else {
                   print('no permission provided');
                 }
               })
-          
         ],
       ),
     );
   }
 }
+
 final picker = ImagePicker();
 
-  void showImagePicker(BuildContext context) {
-    showModalBottomSheet(
-        context: context,
-        builder: (builder){
-          return Card(
-            child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height/5.2,
-                margin: const EdgeInsets.only(top: 8.0),
-                padding: const EdgeInsets.all(12),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                        child: InkWell(
-                          child: Column(
-                            children: const [
-                              Icon(Icons.image, size: 60.0,),
-                              SizedBox(height: 12.0),
-                              Text(
-                                "Gallery",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(fontSize: 16, color: Colors.black),
-                              )
-                            ],
+void showImagePicker(BuildContext context) {
+  showModalBottomSheet(
+      context: context,
+      builder: (builder) {
+        return Card(
+          child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 5.2,
+              margin: const EdgeInsets.only(top: 8.0),
+              padding: const EdgeInsets.all(12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                      child: InkWell(
+                    child: Column(
+                      children: const [
+                        Icon(
+                          Icons.image,
+                          size: 60.0,
+                        ),
+                        SizedBox(height: 12.0),
+                        Text(
+                          "Gallery",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 16, color: Colors.black),
+                        )
+                      ],
+                    ),
+                    onTap: () {
+                      _imgFromGallery();
+                      Navigator.pop(context);
+                    },
+                  )),
+                  Expanded(
+                      child: InkWell(
+                    child: SizedBox(
+                      child: Column(
+                        children: const [
+                          Icon(
+                            Icons.camera_alt,
+                            size: 60.0,
                           ),
-                          onTap: () {
-                            _imgFromGallery();
-                            Navigator.pop(context);
-                          },
-                        )),
-                    Expanded(
-                        child: InkWell(
-                          child: SizedBox(
-                            child: Column(
-                              children: const [
-                                Icon(Icons.camera_alt, size: 60.0,),
-                                SizedBox(height: 12.0),
-                                Text(
-                                  "Camera",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(fontSize: 16, color: Colors.black),
-                                )
-                              ],
-                            ),
-                          ),
-                          onTap: () {
-                            _imgFromCamera();
-                            Navigator.pop(context);
-                          },
-                        ))
-                  ],
-                )),
-          );
-        }
-    );
-  }
-   _imgFromGallery() async {
-    await  picker.pickImage(
-        source: ImageSource.gallery, imageQuality: 50
-    ).then((value){
-      if(value != null){
-        
-      }
-    });
-  }
+                          SizedBox(height: 12.0),
+                          Text(
+                            "Camera",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 16, color: Colors.black),
+                          )
+                        ],
+                      ),
+                    ),
+                    onTap: () {
+                      _imgFromCamera();
+                      Navigator.pop(context);
+                    },
+                  ))
+                ],
+              )),
+        );
+      });
+}
 
-  _imgFromCamera() async {
-    await picker.pickImage(
-        source: ImageSource.camera, imageQuality: 50
-    ).then((value){
-      if(value != null){
-       
-      }
-    });
-  }
+_imgFromGallery() async {
+  await picker
+      .pickImage(source: ImageSource.gallery, imageQuality: 50)
+      .then((value) {
+    if (value != null) {}
+  });
+}
+
+_imgFromCamera() async {
+  await picker
+      .pickImage(source: ImageSource.camera, imageQuality: 50)
+      .then((value) {
+    if (value != null) {}
+  });
+}
